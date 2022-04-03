2c2bb9942f56b98e6fec57ca2857821ccd2c221f
MingMing <greeteaz567@gmail.com>
2022-04-02 21:05:17 -0500

    Finished home page

--- src/home.js
+++ src/home.js
@@ -8,10 +8,18 @@ homepage.appendChild(about);
 
 const title = document.createElement("div");
 title.classList.add("abouttitle");
+title.textContent="About";
 
 const content = document.createElement("div");
 content.classList.add("aboutcontent");
+content.textContent=`This is a very safe restaurant with very safe foods. We serve you 
+the best we can and help out with your budget. We are very keen to 
+satisify our customers taste and needs. We ha--- error error error
+error error error error error error error error error error error 
+error error error error error error error error error error error
+error error error error error`;
 
 about.appendChild(title);
 about.appendChild(content);
 
+export default homepage;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import spawn from "./home.js"
 
+const content = document.querySelector("#content");
+content.appendChild(spawn);
 
+console.log(spawn);
